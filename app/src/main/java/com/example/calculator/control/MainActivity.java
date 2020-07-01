f1a505cbbd0f6eea1348559f0f5d2ef53a1b1a4b
sulai.ji <sulai.ji@sim.com>
2020-07-01 16:40:33 +0800

    []

---
+++ app/src/main/java/com/example/calculator/control/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculator.control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calculator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
